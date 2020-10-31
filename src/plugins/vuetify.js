0703323c16b189eaa18faaf0ed952f7098226fec
Achilles Moraites <achimoraites@yahoo.gr>
2020-10-31 23:26:04 +0200

    feat: Added SearchBox
    - Configured mdi icons
    - Created new SearchBox Component

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  iconfont: 'mdi',
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="home">
-
-  </div>
+  <v-container mt-5 pt-5>
+    <SearchBox
+      :label="label"
+      @onSearch="onSearch"
+     />
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
+import SearchBox from '@/components/SearchBox.vue';
 
 export default {
   name: 'Home',
   components: {
-
+    SearchBox,
+  },
+  data: () => ({
+    label: 'Search by Title',
+  }),
+  methods: {
+    onSearch(value) {
+      console.log(value);
+    },
   },
 };
 </script>
