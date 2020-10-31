c7074f842e33299fc3ec2afabc10c642548ab063
Achilles Moraites <achimoraites@yahoo.gr>
2020-10-31 22:07:14 +0200

    feat: Basic project setup
    Preparations for starting project development
    - Basic Project structure
    - Added Vuetify
    - Routing
    - Vuex
    - .env

---
+++ src/layout/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+ <v-app-bar app>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <h1>Movie Finder</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="d-flex align-center justify-center">
+        <v-switch
+          v-model="$vuetify.theme.dark"
+          inset
+          :label="themeLabel"
+        ></v-switch>
+      </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  computed: {
+    themeLabel() {
+      return this.$vuetify.theme.dark ? 'Toggle Light Theme' : 'Toggle Dark Theme';
+    },
+  },
+};
+</script>
